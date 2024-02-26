a6ca0f2761bc5567a88a9aaef27666b3f9d4062a
XakHD <zcheldercraftllc@gmail.com>
2024-02-26 09:30:17 -0500

    Create medium-zigzag-conversion.js

---
+++ medium-zigzag-conversion.js
@@ -0,0 +1,64 @@
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+
+// string convert(string s, int numRows);
+ 
+
+// Example 1:
+
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+// Example 2:
+
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+// Example 3:
+
+// Input: s = "A", numRows = 1
+// Output: "A"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of English letters (lower-case and upper-case), ',' and '.'.
+// 1 <= numRows <= 1000
+
+//My Solution 
+var convert = function(s, numRows) {
+    // Handle the special case of numRows being 1
+    if (numRows === 1) return s;
+
+    let rows = [];
+    for (let i = 0; i < Math.min(numRows, s.length); i++) {
+        rows[i] = "";
+    }
+
+    let currentRow = 0;
+    let goingDown = false;
+
+    for (let c of s) {
+        rows[currentRow] += c;
+        if (currentRow === 0 || currentRow === numRows - 1) {
+            goingDown = !goingDown;
+        }
+        currentRow += goingDown ? 1 : -1;
+    }
+
+    let zigzag = "";
+    for (let row of rows) {
+        zigzag += row;
+    }
+    return zigzag;
+};
