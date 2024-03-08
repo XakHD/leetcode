98f66cb355511f48377a259851112ac94d5332d0
XakHD <zcheldercraftllc@gmail.com>
2024-03-07 22:44:48 -0500

    Create medium-string-to-integer-atoi.js

---
+++ medium-string-to-integer-atoi.js
@@ -0,0 +1,116 @@
+
+// Code
+// Testcase
+// Testcase
+// Test Result
+// 8. String to Integer (atoi)
+// Medium
+// Topics
+// Companies
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+// The algorithm for myAtoi(string s) is as follows:
+
+// Read in and ignore any leading whitespace.
+// Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+// Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+// Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+// If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+// Return the integer as the final result.
+// Note:
+
+// Only the space character ' ' is considered a whitespace character.
+// Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ 
+
+// Example 1:
+
+// Input: s = "42"
+// Output: 42
+// Explanation: The underlined characters are what is read in, the caret is the current reader position.
+// Step 1: "42" (no characters read because there is no leading whitespace)
+//          ^
+// Step 2: "42" (no characters read because there is neither a '-' nor '+')
+//          ^
+// Step 3: "42" ("42" is read in)
+//            ^
+// The parsed integer is 42.
+// Since 42 is in the range [-231, 231 - 1], the final result is 42.
+// Example 2:
+
+// Input: s = "   -42"
+// Output: -42
+// Explanation:
+// Step 1: "   -42" (leading whitespace is read and ignored)
+//             ^
+// Step 2: "   -42" ('-' is read, so the result should be negative)
+//              ^
+// Step 3: "   -42" ("42" is read in)
+//                ^
+// The parsed integer is -42.
+// Since -42 is in the range [-231, 231 - 1], the final result is -42.
+// Example 3:
+
+// Input: s = "4193 with words"
+// Output: 4193
+// Explanation:
+// Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+//          ^
+// Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+//          ^
+// Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+//              ^
+// The parsed integer is 4193.
+// Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 200
+// s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
+
+//MY Solution
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (str) {
+    // Base condition
+    if (!str) {
+        return 0;
+    }
+    // MAX and MIN values for integers
+    const INT_MAX = 2147483647;
+    const INT_MIN = -2147483648;
+    // Trimmed string
+    str = str.trim();
+    // Counter
+    let i = 0;
+    // Flag to indicate if the number is negative
+    const isNegative = str[0] === '-';
+    // Flag to indicate if the number is positive
+    const isPositive = str[0] === '+';
+    if (isNegative) {
+        i++;
+    } else if (isPositive) {
+        i++;
+    }
+    // This will store the converted number
+    let number = 0;
+    // Loop for each numeric character in the string iff numeric characters are leading
+    // characters in the string
+    while (i < str.length && str[i] >= '0' && str[i] <= '9') {
+        number = number * 10 + (str[i] - '0');
+        i++;
+    }
+    // Give back the sign to the converted number
+    number = isNegative ? -number : number;
+    if (number < INT_MIN) {
+        return INT_MIN;
+    }
+    if (number > INT_MAX) {
+        return INT_MAX;
+    }
+    return number;
+};
